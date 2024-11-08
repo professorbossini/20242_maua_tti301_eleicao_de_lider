bad6f59751aeaa5875a032a5689edb66d991c40d
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-08 09:00:42 -0300

    t1: ap01(zookeeper): encerra a apostila

--- src/main/java/br/bossini/EleicaoDeLider.java
+++ src/main/java/br/bossini/EleicaoDeLider.java
@@ -1,6 +1,8 @@
 package br.bossini;
 
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+
 import java.io.IOException;
 
 public class EleicaoDeLider {
@@ -8,17 +10,37 @@ public class EleicaoDeLider {
     private static final String PORTA = "2181";
     private static final int TIMEOUT = 5000;
     private ZooKeeper zooKeeper;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var eleicaoDeLider = new EleicaoDeLider();
+        eleicaoDeLider.conectar();
+        eleicaoDeLider.executar();
     }
 
-    public void conectar() throws IOException {
+    public void conectar() throws IOException{
         zooKeeper = new ZooKeeper(
             String.format("%s:%s", HOST, PORTA),
             TIMEOUT,
             (evento) -> {
-
+                if(evento.getType() == Watcher.Event.EventType.None){
+                    if(evento.getState() == Watcher.Event.KeeperState.SyncConnected){
+                        System.out.println("Conectou");
+                    }
+                    else if(evento.getState() == Watcher.Event.KeeperState.Disconnected){
+                        synchronized (zooKeeper){
+                            System.out.println("Desconectou...\n");
+                            System.out.printf("Estamos na thread: %s\n", Thread.currentThread().getName());
+                            zooKeeper.notify();
+                        }
+                    }
+                }
             }
         );
+    }
+
+    public void executar() throws InterruptedException{
+        synchronized (zooKeeper){
+            zooKeeper.wait();
+        }
     }
 }
