689d9d28ceeee6b5ed45e8fb9b48bc802f2d07ec
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-13 10:56:14 -0300

    t1: ap02(zookeeper): encerra a apostila

--- src/main/java/br/bossini/EleicaoDeLider.java
+++ src/main/java/br/bossini/EleicaoDeLider.java
@@ -1,20 +1,51 @@
 package br.bossini;
 
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class EleicaoDeLider {
-    private static final String HOST = "localhost";
+    private static final String HOST = "172.20.10.2";
     private static final String PORTA = "2181";
-    private static final int TIMEOUT = 5000;
+    private static final int TIMEOUT = 50000;
+    private static final String NAMESPACE_ELEICAO = "/eleicao";
     private ZooKeeper zooKeeper;
+    private String nomeDoZNodeDesseProcesso;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         var eleicaoDeLider = new EleicaoDeLider();
         eleicaoDeLider.conectar();
+        eleicaoDeLider.realizarCandidatura();
+        eleicaoDeLider.elegerOLider();
         eleicaoDeLider.executar();
+        eleicaoDeLider.fechar();
+    }
+
+    public void fechar() throws InterruptedException{
+        zooKeeper.close();
+    }
+
+    public void realizarCandidatura() throws InterruptedException, KeeperException{
+        String prefixo = String.format("%s/cand_", NAMESPACE_ELEICAO);
+        String pathInteiro = zooKeeper.create(
+            prefixo, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL
+        );
+        System.out.println(pathInteiro);
+        nomeDoZNodeDesseProcesso = pathInteiro.replace(String.format("%s/", NAMESPACE_ELEICAO), "");
+    }
+
+    public void elegerOLider () throws InterruptedException, KeeperException{
+        List<String> candidatos = zooKeeper.getChildren(NAMESPACE_ELEICAO, false);
+        Collections.sort(candidatos);
+        String oMenor = candidatos.get(0);
+        if(oMenor.equals(nomeDoZNodeDesseProcesso)){
+            System.out.printf("Me chamo %s e sou o líder\n", nomeDoZNodeDesseProcesso);
+        }
+        else{
+            System.out.printf("Me chamo %s e não sou o líder. O líder é o %s", nomeDoZNodeDesseProcesso , oMenor);
+        }
     }
 
     public void conectar() throws IOException{
